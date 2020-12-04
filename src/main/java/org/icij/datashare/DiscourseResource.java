2dfe602e6658327d7416d2f18ff19605fba759fb
Maxime Vanza <maximevanza@live.fr>
2020-12-04 16:55:39 +0000

    [ICIJ/datashare#652] implementation of proxy put and post

--- src/main/java/org/icij/datashare/DiscourseResource.java
+++ src/main/java/org/icij/datashare/DiscourseResource.java
@@ -2,13 +2,22 @@ package org.icij.datashare;
 
 import net.codestory.http.Context;
 import net.codestory.http.annotations.Get;
+import net.codestory.http.annotations.Post;
 import net.codestory.http.annotations.Prefix;
+import net.codestory.http.annotations.Put;
+import net.codestory.http.errors.UnauthorizedException;
 import net.codestory.http.payload.Payload;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.BasicHttpEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.icij.datashare.user.User;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,21 +27,64 @@ public class DiscourseResource {
 
     private final URL discourseUrl;
     private final HttpClient httpClient;
+    private final String discourseApiKey;
 
     public DiscourseResource(PropertiesProvider propertiesProvider) throws MalformedURLException {
        discourseUrl = new URL(propertiesProvider.get("discourseUrl").orElse("http://discourse:3000"));
+       discourseApiKey = propertiesProvider.get("discourseApiKey").orElseThrow(() -> new RuntimeException("no discourse api key found in settings"));
        httpClient = HttpClientBuilder.create().build();
     }
 
     @Get("/:project/:url:")
     public Payload getMethod(String project, String url, Context context) throws IOException {
+        checkProject(project,context);
         try {
             HttpGet httpUriRequest = new HttpGet(discourseUrl.toString() + "/" + url);
+            prepareRequest(httpUriRequest,context.currentUser().login());
             HttpResponse response = httpClient.execute(httpUriRequest);
-            return new Payload(response.getFirstHeader("Content-Type").toString(), response.getEntity().getContent(), response.getStatusLine().getStatusCode());
+            return new Payload(response.getFirstHeader("Content-Type").toString(), response.getEntity().getContent(), response.getStatusLine().getStatusCode()).withHeader("Api-Username",context.currentUser().login());
         }catch (Exception e){
             e.printStackTrace();
             throw new RuntimeException(e);
+        }
+    }
+
+    @Put("/:project/:url:")
+    public Payload putMethod(String project, String url, Context context) throws IOException {
+        checkProject(project,context);
+        return getPayload(context, new HttpPut(discourseUrl.toString() + "/" + url));
+    }
+
+    @Post("/:project/:url:")
+    public Payload postMethod(String project, String url, Context context) throws IOException {
+        checkProject(project,context);
+        return getPayload(context, new HttpPost(discourseUrl.toString() + "/" + url));
+    }
+
+    private Payload getPayload(Context context, HttpEntityEnclosingRequestBase httpUriRequest) throws IOException {
+        BasicHttpEntity entity = new BasicHttpEntity();
+        entity.setContentType(new BasicHeader("Content-Type", context.header("Content-Type")));
+        byte[] contentAsBytes = context.request().contentAsBytes();
+        entity.setContent(new ByteArrayInputStream(contentAsBytes));
+        entity.setContentLength(contentAsBytes.length);
+        httpUriRequest.setEntity(entity);
+        prepareRequest(httpUriRequest, context.currentUser().login());
+        HttpResponse response = httpClient.execute(httpUriRequest);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        IOUtils.copy(response.getEntity().getContent(), output);
+        String payload = output.toString();
+        return new Payload(response.getFirstHeader("Content-Type").toString(), payload, response.getStatusLine().getStatusCode());
+    }
+
+    private void prepareRequest(HttpUriRequest httpUriRequest, String login) {
+        httpUriRequest.addHeader("Api-Key", discourseApiKey);
+        httpUriRequest.addHeader("Api-Username", login);
+        httpUriRequest.addHeader("Content-Type", "application/json");
+    }
+
+    private void checkProject(String project, Context context) {
+        if (!((User)context.currentUser()).isGranted(project)) {
+            throw new UnauthorizedException();
         }
     }
 }
--- src/test/java/org/icij/datashare/DiscourseResourceTest.java
+++ src/test/java/org/icij/datashare/DiscourseResourceTest.java
@@ -1,7 +1,9 @@
 package org.icij.datashare;
 
+import net.codestory.http.errors.UnauthorizedException;
 import net.codestory.http.filters.basic.BasicAuthFilter;
 import net.codestory.http.security.Users;
+import org.icij.datashare.session.LocalUserFilter;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -17,8 +19,13 @@ public class DiscourseResourceTest extends AbstractProdWebServerTest{
     public static void setUpDiscourse() {
         discourse.configure(routes -> routes
                 .get("/my/url", new HashMap<String, String>() {{
-                    put("Test", "Value");
-                }}));
+                    put("Test", "Get");
+                }})
+                .put("/my/url",(context -> new HashMap<String,String>(){{
+                    put("Test","Put");
+                }}))
+                .post("/my/url",(context -> new HashMap<String,String>(){{
+                    put("Test","Post");}})));
     }
     @Before
     public void setUp() throws MalformedURLException {
@@ -26,11 +33,43 @@ public class DiscourseResourceTest extends AbstractProdWebServerTest{
             put("discourseUrl", "http://localhost:" + discourse.port());
             put("discourseApiKey", "testApiKey");
         }}));
-        configure(routes -> routes.add(discourseResource).filter(new BasicAuthFilter("/api","ds", Users.singleUser("foo","bar"))));
+        configure(routes -> routes.add(discourseResource).filter(new BasicAuthFilter("/api","ds", DatashareUser.singleUser("foo"))));
     }
 
     @Test
     public void test_get() {
-        get("/api/proxy/projectid/my/url").withPreemptiveAuthentication("foo","bar").should().respond(200).contain("\"Test\":\"Value\"");
+        get("/api/proxy/foo-datashare/my/url").withPreemptiveAuthentication("foo","null").should().respond(200).contain("\"Test\":\"Get\"");
+    }
+
+    @Test
+    public void test_unauthorized_user() {
+        get("/api/proxy/foo-datashare/my/url").withPreemptiveAuthentication("baz","null").should().respond(401);
+    }
+
+    @Test
+    public void test_unknown_project() {
+        get("/api/proxy/unknown_project/my/url").withPreemptiveAuthentication("foo","null").should().respond(401);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test_no_api_key() throws MalformedURLException {
+        DiscourseResource discourseResource = new DiscourseResource(new PropertiesProvider());
+        configure(routes -> routes.add(discourseResource).filter(new BasicAuthFilter("/api","ds", DatashareUser.singleUser("foo"))));
+        get("/api/proxy/foo-datashare/my/url").withPreemptiveAuthentication("foo","null");
+    }
+
+//    @Test
+//    public void test_user_in_discourse_response_header() {
+//        get("/api/proxy/foo-datashare/my/url").withPreemptiveAuthentication("foo","bar").should().haveHeader("Api-Username","foo");
+//    }
+
+    @Test
+    public void test_put() {
+        put("/api/proxy/foo-datashare/my/url").withPreemptiveAuthentication("foo","null").should().respond(200).contain("\"Test\":\"Put\"");
+    }
+
+    @Test
+    public void test_post() {
+        post("/api/proxy/foo-datashare/my/url").withPreemptiveAuthentication("foo","null").should().respond(200).contain("\"Test\":\"Post\"");
     }
 }
